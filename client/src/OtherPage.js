52bb1f2b1868c1a9ca9ceff3d08ef873804a2ef9
Malpka8 <bszabat5@gmail.com>
2023-01-30 20:47:11 +0100

    First commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default () => {
+  return (
+    <div>
+      <p>multifib</p>
+      <p>Bartłomiej Szabat</p>
+      <p>I2S 2.4</p>
+      
+    </div>
+  );
+};
