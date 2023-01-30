b8defdf2e0b3a6175dfa19b6a8082d143cd0fc90
Malpka8 <bszabat5@gmail.com>
2023-01-30 21:11:11 +0100

    Poprawki

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
     <div>
-      <p>multifib</p>
+      <p>BSmultifib</p>
       <p>Bartłomiej Szabat</p>
       <p>I2S 2.4</p>
-      
+      <Link to="/">Go back home</Link>
     </div>
   );
 };
